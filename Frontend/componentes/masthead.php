e38d56aeee9fdc7a637276fb32bec1fddbb4e83b
Joisi <joisi.alcazar@unireformada.edu.co>
2022-06-29 21:34:39 -0500

    cambio total

--- Frontend/componentes/masthead.php
+++
@@ -1,8 +0,0 @@
- <!-- Masthead-->
- <header class="masthead">
-            <div class="container">
-                <div class="masthead-subheading">¡Bienvenido a nuestra web!</div>
-                <div class="masthead-heading text-uppercase">Galería del Mueble</div>
-                <a class="btn btn-primary btn-xl text-uppercase" style= "background-color: #ffc800"; href="#about">Dime más</a>
-            </div>
-        </header>
